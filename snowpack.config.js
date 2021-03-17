eef0c99883f922cf49e931e12bdf3fef73d3656e
Lachlan Arthur <git@me.lach.la>
2021-03-18 02:35:49 +1000

    Make electron app

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,25 +1,26 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
-  },
-  plugins: ['@snowpack/plugin-typescript'],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+	mount: {
+		public: { url: '/', static: true },
+		src: { url: '/' },
+	},
+	plugins: [ '@snowpack/plugin-typescript' ],
+	routes: [
+		/* Enable an SPA Fallback in development: */
+		// {"match": "routes", "src": ".*", "dest": "/index.html"},
+	],
+	optimize: {
+		/* Example: Bundle your final build: */
+		// "bundle": true,
+	},
+	packageOptions: {
+		/* ... */
+	},
+	devOptions: {
+		/* ... */
+	},
+	buildOptions: {
+		baseUrl: './build/',
+		clean: true,
+	},
 };
